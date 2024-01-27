5d59f959c9ce41f2a487570c77908d5e3beed1d8
727721euit096-NALLASIVAM <125950986+727721euit096-NALLASIVAM@users.noreply.github.com>
2024-01-27 09:37:41 +0530

    Add files via upload

---
+++ Frontend/Day 3/src/App.jsx
@@ -0,0 +1,18 @@
+import Register from "./pages/Register";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import  Home  from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
