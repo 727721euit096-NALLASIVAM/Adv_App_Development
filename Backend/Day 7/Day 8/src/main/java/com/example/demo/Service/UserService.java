f0af2d8977f1ac5206d1b9736e1e030b1d39b1b2
727721euit096-NALLASIVAM <125950986+727721euit096-NALLASIVAM@users.noreply.github.com>
2024-02-09 10:13:11 +0530

    Delete Backend/Day 7/Day 8 directory

--- Backend/Day 7/Day 8/src/main/java/com/example/demo/Service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Service;
-
-import com.example.demo.Entity.User;
-import com.example.demo.dto.UserDTO;
-
-public interface UserService {
-    User register(UserDTO userDTO);
-    User login(String emailId, String password);
-}
