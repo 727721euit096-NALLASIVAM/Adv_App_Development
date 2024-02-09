ae93e196c7c86d3ccd0e70bf5233155e4177c0fe
727721euit096-NALLASIVAM <125950986+727721euit096-NALLASIVAM@users.noreply.github.com>
2024-02-09 10:22:57 +0530

    Add files via upload

---
+++ Backend/Day 9/src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,56 @@
+package com.example.demo.dto;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String phone;
+    private int age;
+    private String gender;
+    private String password;
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // getters and setters
+
+}
